48209d458f96dc0cbdc1721823d0f63951395853
Tom Pettit <33840098+tom-pettit@users.noreply.github.com>
2023-04-28 19:26:16 +0100

    Q2 Draft V1

--- problem2.py
+++ problem2.py
@@ -24,6 +24,7 @@ new_grid = xr.DataArray(
 
 color_dataarray =  rioxarray.open_rasterio('./dataset/color_dataset/lroc_color_poles_4k.tif')
 color_tif_shape = color_dataarray.shape
+
 color_dataarray = color_dataarray.isel(band=0)
 import xarray as xr
 import numpy as np 
@@ -81,11 +82,6 @@ zoomed_fig.grdview(
 
 zoomed_fig.colorbar(frame=["a1", "x+lElevation", "y+lm"])
 zoomed_fig.savefig('tempfig.png')
-
-zoomed_flat_fig = pygmt.Figure()
-zoomed_flat_fig.grdimage(grid=new_grid, cmap=cmap, projection="Q12C", region=region)
-
-zoomed_flat_fig.savefig('tempflatfig.png')
 import cv2 
 
 img = cv2.imread('wholefig.png')
@@ -96,9 +92,6 @@ img = cv2.imread('tempfig.png')
 resized = cv2.resize(img, (600,300))
 cv2.imwrite('tempfig.png', resized)
 
-img = cv2.imread('tempflatfig.png')
-resized = cv2.resize(img, (600,300))
-cv2.imwrite('tempflatfig.png', resized)
 
 def change_view():
     import os
@@ -155,14 +148,7 @@ def change_zoomed_region(region):
         surftype="s",
         cmap=cmap
     )
-    zoomed_fig.colorbar(frame=["a1", "x+lElevation", "y+lm"])
-    zoomed_flat_fig = pygmt.Figure()
-    zoomed_flat_fig.grdimage(grid=new_grid, cmap=cmap, projection="Q12C", region=region)
 
-    zoomed_flat_fig.savefig('tempflatfig.png')
-    img = cv2.imread('tempflatfig.png')
-    resized = cv2.resize(img, (600,300))
-    cv2.imwrite('tempflatfig.png', resized)
 
     zoomed_fig.savefig('tempfig.png')
 
@@ -183,18 +169,19 @@ whole_graph = sg.Graph(canvas_size=(600,300), graph_bottom_left=(0, 0), graph_to
 cmaps = ['oleron', 'vic', 'gray', 'acton', 'oslo', 'roma']
 
 layout = [
-    [sg.Text("Whole Moon Surface")],
-    [sg.Text("Draw a rectangle over a region to zoom in")],
+    [sg.Image('./logo.png', background_color='#060b27')],
+    [sg.Image('./rotating_moon2.gif', key = "MoonGIF", background_color='#060b27')],
+    [sg.Text("Combatting the complex terrain, to take the next wave of humans to the moon.", font=('Avenir', 25), background_color='#060b27')],
+    [sg.Text("The picture below shows the surface of our moon. Its uneven terrain is tricky to land a rocket. Have a look for yourself, by drawing a rectangle over a region to zoom in!", font=('Avenir', 15), background_color='#060b27')],
     [whole_graph, sg.Graph(canvas_size=(600,300), graph_bottom_left=(0, 0), graph_top_right=(600,300), enable_events=True, drag_submits=True, key='ZoomedGraph')],
-    [sg.Text('Change the height of the 3D view',font=('Arial Bold', 12)), 
-        sg.Button("Higher"), sg.Button('Lower')
+    [sg.Text('Change the height of the 3D view',font=('Avenir', 12), background_color='#060b27'), 
+        sg.Button("Higher"), sg.Button('Lower'), sg.Button('Left'), sg.Button('Right')
     ],
-    [sg.Text("Left: Zoomed In Area"), sg.Text("Right: Flat Version of Zoomed In Area")],
-    [
-     sg.Graph(canvas_size=(600,300), graph_bottom_left=(0, 0), graph_top_right=(600,300), enable_events=True, drag_submits=True, key='ZoomedFlatGraph')
-     ],
-    [sg.Text("Click on the flat version of the zoomed in area to sample point heights.")],
-    [sg.Text('Longitude: '+"{:.3f}".format(selected_long)+' Latitude: '+"{:.3f}".format(selected_lat)+' Height: '+"{:.3f}".format(float(selected_point_height))+'m',font=('Arial Bold', 20), key='ChosenPointHeight', justification='left')]
+    [sg.Text("Why are we doing this?", font=('Avenir', 30), background_color='#060b27')],
+    [sg.Text("1. We’re building on more than 50 years of exploration experience to reignite America’s passion for discovery.", font=('Avenir', 18), background_color='#060b27')],
+    [sg.Text("2. Artemis missions enable a growing lunar economy by fueling new industries, supporting job growth, and furthering the demand for a skilled workforce..", font=('Avenir', 18), background_color='#060b27')],
+    [sg.Text("3. Inspiring the next generation", font=('Avenir', 18), background_color='#060b27')]
+    
 ]
 
 # Create the form and show it without the plot
@@ -204,12 +191,12 @@ window = sg.Window(
     location=(0, 0),
     finalize=True,
     element_justification="center",
-    font="Helvetica 18",
+    font="Avenir 18",
+    background_color='#060b27'
 )
 
 window['WholeGraph'].draw_image(filename='./wholefig.png', location=(0,300))
 window['ZoomedGraph'].draw_image(filename='./tempfig.png', location=(0,300))
-window['ZoomedFlatGraph'].draw_image(filename='./tempflatfig.png', location=(0,300))
 minlong, maxlong, minlat, maxlat = region.split('/')
 
 xmin = (float(minlong)/0.6) + 300
@@ -223,7 +210,7 @@ dragging = False
 start_point = end_point = prior_rect = None
 
 while True:
-   event, values = window.read()
+   event, values = window.read(timeout=50)
    if event == 'Higher':
       azimuth, elevation = perspective[0], perspective[1]
       azimuth = int(azimuth)
@@ -251,6 +238,30 @@ while True:
 
       window.refresh()
 
+   if event == 'Left':
+      azimuth, elevation = perspective[0], perspective[1]
+      azimuth = int(azimuth) + 20
+      elevation = int(elevation) 
+      
+      perspective = [azimuth, elevation]
+      change_view()
+
+      window['ZoomedGraph'].draw_image(filename='./tempfig.png', location=(0,300))
+
+      window.refresh()
+
+   if event == 'Right':
+      azimuth, elevation = perspective[0], perspective[1]
+      azimuth = int(azimuth) - 20
+      elevation = int(elevation) 
+      
+      perspective = [azimuth, elevation]
+      change_view()
+
+      window['ZoomedGraph'].draw_image(filename='./tempfig.png', location=(0,300))
+
+      window.refresh()
+
    elif event == "WholeGraph":  # if there's a "Graph" event, then it's a mouse
         print('click: ', values[event])
         whole_graph.delete_figure(starting_rect)
@@ -264,36 +275,6 @@ while True:
             whole_graph.delete_figure(prior_rect)
         if None not in (start_point, end_point):
             prior_rect = whole_graph.draw_rectangle(start_point, end_point, line_color='red')
-
-   elif event == 'ZoomedFlatGraph':
-       x, y = values[event] 
-
-       window['ZoomedFlatGraph'].erase()
-       window['ZoomedFlatGraph'].draw_image(filename='./tempflatfig.png', location=(0,300))
-       window['ZoomedFlatGraph'].draw_point((x,y), size=15, color='red')
-
-       xmin, xmax, ymin, ymax = region.split('/')
-
-       xmin, xmax, ymin, ymax = float(xmin), float(xmax), float(ymin), float(ymax) 
-
-       actual_width = xmax - xmin
-       actual_height = ymax - ymin
-
-       long = (x / 600) * actual_width + xmin 
-       lat = (y / 300) * actual_height + ymin 
-
-       def find_nearest(array, value):
-          array = np.asarray(array)
-          idx = (np.abs(array - value)).argmin()
-          return array[idx]
-       
-       selected_long = long 
-       selected_lat = lat
-
-       selected_point_height = new_grid.sel(x = selected_long, y = selected_lat, method = 'nearest').data
-
-       window['ChosenPointHeight'].update('Longitude: '+"{:.3f}".format(selected_long)+' Latitude: '+"{:.3f}".format(selected_lat)+' Height: '+"{:.3f}".format(float(selected_point_height))+'m')
-
 
    elif event == 'WholeGraph+UP':  
        if start_point is not None and end_point is not None:
@@ -323,11 +304,13 @@ while True:
 
         change_zoomed_region(region)
         window['ZoomedGraph'].draw_image(filename='./tempfig.png', location=(0,300))
-        window['ZoomedFlatGraph'].draw_image(filename='./tempflatfig.png', location=(0,300))
 
         window.refresh()
         start_point, end_point = None, None 
         dragging = False
+
+   window["MoonGIF"].UpdateAnimation("./rotating_moon2.gif",time_between_frames=50)
+
 
   
    if event == sg.WIN_CLOSED or event == 'Exit':
